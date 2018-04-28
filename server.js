61d4da83010a88d4755526c23fef3850a9bf7da5
UMANG-GAKHAR <37589983+UMANG-GAKHAR@users.noreply.github.com>
2018-04-28 12:02:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var Crypto = require('Crypto');
+var config ={
+    user:'umangakhar29',
+    database:'umangakhar29',
+    host:'ssh.imad.hasura-app.io',
+    port:'5432',
+    password:db-umangakhar29-35877
+};
 
 var app = express();
 app.use(morgan('combined'));
 
+function createTemplate(data){}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function hash (input, salt){
+    // How do we create a hash?
+    var hashed =crypto.pbkdf2Sync(input, salt, 10000 ,512, 'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+})
+
 var counter=0;
 app.get('/counter',function(req,res){
  counter =counter + 1;
